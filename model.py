af7e93c77004a38af0144e1dadc8e9a0a79452dd
Victor Mas Montserrat <victormasmon@gmail.com>
2024-07-19 17:53:37 +0200

    add func allocate

--- model.py
+++ model.py
@@ -1,23 +1,36 @@
 from datetime import date
 from dataclasses import dataclass
-from typing import Optional, Union
+from typing import Optional, NewType, List
+
+
+Quantity = NewType("Quantity", int)
+Sku = NewType("Sku", str)
+Reference = NewType("Reference", str)
 
 
 @dataclass(frozen=True)
 class OrderLine:
     order_id: str
     sku: str
-    quantity: int
+    quantity: Quantity
 
 
 class Batch:
     def __init__(
-        self, ref: str, sku: str, qty: int, eta: Optional[date] = None
+        self, ref: Reference, sku: Sku, qty: Quantity, eta: Optional[date] = None
     ) -> None:
         self.__reference = ref
         self.__sku = sku
-        self.__eta = eta
         self.__available_quantity = qty
+        self.__eta = eta
+        self.__allocated_lines = set()
+
+    def __gt__(self, other: 'Batch') -> bool:
+        if self.__eta is None:
+            return False
+        elif other.__eta is None:
+            return True
+        return self.__eta > other.__eta
 
     @property
     def reference(self):
@@ -35,14 +48,36 @@ class Batch:
     def available_quantity(self):
         return self.__available_quantity
 
-    def can_allocate(self, line: OrderLine) -> Union[ValueError, bool]:
-        if line.sku is not self.sku:
-            return ValueError("The sku doesn't match")
-        elif line.quantity > self.available_quantity:
-            return ValueError("The order line asks for more quantity than available")
-        return True
-
-    def allocate(self, line: OrderLine) -> int:
-        if error := self.can_allocate(line) is not True:
+    def allocate(self, line: OrderLine) -> None:
+        if error := self.can_allocate(line):
             raise error
         self.__available_quantity -= line.quantity
+        self.__allocated_lines.add(line)
+
+    def can_allocate(self, line: OrderLine) -> Optional[ValueError]:
+        if line.sku is not self.__sku:
+            return ValueError("The sku doesn't match")
+        elif line.quantity > self.__available_quantity:
+            return ValueError("The order line asks for more quantity than available")
+        elif line in self.__allocated_lines:
+            return ValueError("The line has already been allocated")
+
+    def deallocate(self, line: OrderLine) -> None:
+        if error := self.can_deallocate(line):
+            raise error
+
+        self.__available_quantity += line.quantity
+        self.__allocated_lines.remove(line)
+
+    def can_deallocate(self, line: OrderLine) -> Optional[ValueError]:
+        if line not in self.__allocated_lines:
+            return ValueError("The order line hasn't been allocated")
+
+
+def allocate(line: OrderLine, batches: List[Batch]) -> Batch:
+    sel_batch = next(
+        batch for batch in sorted(batches) if not isinstance(batch.can_allocate(line), ValueError)
+    )
+
+    sel_batch.allocate(line)
+    return sel_batch
--- test_model.py
+++ test_model.py
@@ -1,7 +1,8 @@
 from datetime import date, timedelta
+
 import pytest
 
-from model import Batch, OrderLine
+from model import Batch, OrderLine, allocate
 
 
 today = date.today()
@@ -11,11 +12,7 @@ later = tomorrow + timedelta(days=10)
 
 @pytest.fixture
 def default_batch():
-    return Batch(
-        ref="dummy-batch",
-        sku="product-1",
-        qty=10,
-        eta=tomorrow)  
+    return Batch(ref="dummy-batch", sku="product-1", qty=10, eta=tomorrow)
 
 
 def test_allocating_to_a_batch_reduces_the_available_quantity(default_batch: Batch):
@@ -31,16 +28,15 @@ def test_allocating_to_a_batch_reduces_the_available_quantity(default_batch: Bat
 
 def test_can_allocate_if_available_greater_than_required(default_batch: Batch):
     order_line = OrderLine("order-00001", sku=default_batch.sku, quantity=5)
-    assert default_batch.can_allocate(order_line) is True
+    assert not isinstance(default_batch.can_allocate(order_line), ValueError)
 
 
 def test_cannot_allocate_if_available_smaller_than_required(default_batch: Batch):
-    order_line = OrderLine("order-00001", sku=default_batch.sku, quantity=default_batch.available_quantity + 1)
-    assert isinstance(default_batch.can_allocate(order_line), ValueError)
-
-
-def test_cannot_allocate_if_different_sku(default_batch: Batch):
-    order_line = OrderLine("order-00001", sku="product-2", quantity=5)
+    order_line = OrderLine(
+        "order-00001",
+        sku=default_batch.sku,
+        quantity=default_batch.available_quantity + 1,
+    )
     assert isinstance(default_batch.can_allocate(order_line), ValueError)
 
 
@@ -48,20 +44,60 @@ def test_can_allocate_if_available_equal_to_required(default_batch: Batch):
     order_line = OrderLine(
         "order-00001", sku=default_batch.sku, quantity=default_batch.available_quantity
     )
-    assert default_batch.can_allocate(order_line) is True
+    assert not isinstance(default_batch.can_allocate(order_line), ValueError)
 
 
-def test_can_deallocate_allocated_lines(default_batch: Batch):
-    pytest.fail("todo")
+def test_cannot_allocate_if_skus_do_not_match(default_batch: Batch):
+    order_line = OrderLine("order-00001", sku="product-2", quantity=5)
+    assert isinstance(default_batch.can_allocate(order_line), ValueError)
+
+
+def test_prefers_warehouse_batches_to_shipments():
+    stock_in_warehouse = Batch("batch-01", "chair", 1, None)
+    stock_in_shipping = Batch("batch-02", "chair", 1, tomorrow) 
+    order = OrderLine("order-01", "chair", 1)
+
+    allocate(order, [stock_in_warehouse, stock_in_shipping])
+
+    assert stock_in_warehouse.available_quantity == 0
+    assert stock_in_shipping.available_quantity == 1
+
+
+def test_prefers_earlier_batches():
+    stock_arrives_today = Batch("batch-01", "chair", 1, today)
+    stock_arrives_tomorrow = Batch("batch-02", "chair", 1, tomorrow)
+    stock_arrives_later = Batch("batch-02", "chair", 1, later) 
+    order = OrderLine("order-01", "chair", 1)
+
+    allocate(order, [stock_arrives_today, stock_arrives_tomorrow, stock_arrives_later])
+
+    assert stock_arrives_today.available_quantity == 0
+    assert stock_arrives_tomorrow.available_quantity == 1
+    assert stock_arrives_later.available_quantity == 1
+
+
+def test_returns_allocated_batch_ref():
+    stock_arrives_today = Batch("batch-01", "chair", 1, today)
+    stock_arrives_tomorrow = Batch("batch-02", "chair", 1, tomorrow)
+    stock_arrives_later = Batch("batch-02", "chair", 1, later) 
+    order = OrderLine("order-01", "chair", 1)
+
+    allocated_batch = allocate(order, [stock_arrives_today, stock_arrives_tomorrow, stock_arrives_later])
+
+    assert allocated_batch == stock_arrives_today
+
+
+def test_can_only_deallocate_allocated_lines(default_batch: Batch):
+    order_line = OrderLine("order-00001", sku=default_batch.sku, quantity=5)
+    assert isinstance(default_batch.can_deallocate(order_line), ValueError)
+    default_batch.allocate(order_line)
+    assert not isinstance(default_batch.can_deallocate(order_line), ValueError)
 
 
 def test_allocation_is_idempotent(default_batch: Batch):
-    pass
-
-
-def test_prefers_warehouse_batches_to_shipments(default_batch: Batch):
-    pytest.fail("todo")
-
-
-def test_prefers_earlier_batches(default_batch: Batch):
-    pytest.fail("todo")
+    order_line = OrderLine("order-00001", sku=default_batch.sku, quantity=5)
+    default_batch.allocate(order_line)
+    current_qty = default_batch.available_quantity
+    with pytest.raises(ValueError):
+        default_batch.allocate(order_line)
+    assert current_qty == default_batch.available_quantity
